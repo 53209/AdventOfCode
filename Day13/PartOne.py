b85b41faaf77f881193b15e83885f458dfc4270a
Master\Jeffrey <jef.schw@gmail.com>
2017-12-15 19:42:49 +0100

    severity wrong

--- Day13/PartOne.py
+++ Day13/PartOne.py
@@ -1,18 +1,42 @@
 # INPUT
-input = open("input.txt")
+input = open("example_input.txt")
 
-firewall = []
-for _ in range(0, 43):
-    temp_list = []
+firewall = {}
+for _ in range(0, 4):
     n = str(input.readline()).strip().split()
-    temp_list.append(int(n[0]))
-    temp_list.append(int(n[1]))
-    firewall.append(temp_list)
+    firewall[int(n[0])] = int(n[1])
+
+
+for i in range(0, 85):
+    if i not in firewall:
+        firewall[i] = 0
 
 print(firewall)
 
-scanner = 0
-for layer in firewall:
-    range = layer[1]
+severity = 0
+for picosecond in range(0, 7):
+    layerRange = firewall[picosecond]
 
-    #Calculate position of scanner
+    if layerRange == 0:
+        continue
+
+    # Calculate position of scanner when entering layer
+    scannerPos = 0
+    coefficient = 1
+    for _ in range(0, picosecond):
+        scannerPos += 1 * coefficient
+        if scannerPos == (layerRange - 1):
+            coefficient = -1    # returning
+        if scannerPos == 0:
+            coefficient = 1     # forward
+
+    # Next Scanner step
+    next_scannerPos = scannerPos + 1*coefficient
+    # print("Picosecond: %s,\n LayerRange: %s,\n current ScannerPos: %s, \n next ScannerPos: %s" % (picosecond, layerRange, scannerPos, next_scannerPos))
+
+    # Calculate severity
+    if next_scannerPos == 0:    # caught
+        print("Picossecond: %s, \n severity: %s, \n layerrange: %s" % (picosecond, severity, layerRange))
+        severity += picosecond*layerRange
+
+print(severity)
