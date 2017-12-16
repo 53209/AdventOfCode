88cceba0d775d8771cf86fd5010bcf4ac6f145e7
Master\Jeffrey <jef.schw@gmail.com>
2017-12-16 15:50:50 +0100

    Initial commit

---
+++ Day15/PartOne.py
@@ -0,0 +1,12 @@
+'''
+Input:
+Generator A starts with 679
+Generator B starts with 771
+'''
+
+a = 679
+b = 771
+
+print("{0:b}".format(225))
+print("{0:b}".format(225)[3:])
+print("{0:b}".format(225)[:4])
