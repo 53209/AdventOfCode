b18c4f59813565c23e91a206519e7ec0568cb8af
Master\Jeffrey <jef.schw@gmail.com>
2017-12-16 13:00:11 +0100

    Part 1 solved Part 2 output wrong

--- Day13/PartTwo.py
+++ Day13/PartTwo.py
@@ -1,51 +1,31 @@
 # INPUT
-input = open("example_input.txt")
+input = open("input.txt")
 
 firewall = {}
-for _ in range(0, 4):
-# for _ in range(0, 43):
+for _ in range(0, 43):
     n = str(input.readline()).strip().split()
     firewall[int(n[0])] = int(n[1])
 
-for i in range(0, 7):
-# for i in range(0, 85):
+for i in range(0, 85):
     if i not in firewall:
         firewall[i] = 0
 
-# print(firewall)
-
-severities = []
-for delay in range(0, 20):
+scannerPos = 0
+severity = 0
+for delay in range(105919, 30000):
     severity = 0
-    for picosecond in range(0, 7):
-    # for picosecond in range(0, 84):
+    for picosecond in range(0, 84):
 
-        # print("curent Pico: %s" % (picosecond))
         layerRange = firewall[picosecond]
 
         if layerRange == 0:
-            # print("LR0: %s" % (picosecond))
             continue
 
-        # Calculate position of scanner when entering layer
-        scannerPos = 0
-        coefficient = 1
-        for _ in range(0, (picosecond + delay)):
-            scannerPos += 1 * coefficient
-            if scannerPos == (layerRange - 1):
-                coefficient = -1  # returning
-            if scannerPos == 0:
-                coefficient = 1  # forward
+        scannerPos = (picosecond+delay) % ((layerRange-1)*2)
 
-        # Next Scanner step
-        next_scannerPos = scannerPos + 1 * coefficient
-        # print("Pico+Del: %s,\n LayerRange: %s,\n current ScannerPos: %s" % (picosecond+delay, layerRange, scannerPos))
-
-        # Calculate severity
-        if scannerPos == 0:  # caught
+        if scannerPos == 0:
             severity += (picosecond+delay) * layerRange
-            # print(severity)
 
-    severities.append(severity)
-
-print(severities)
+    if severity == 0:
+        first = False
+        print(delay)
