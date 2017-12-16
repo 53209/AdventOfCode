0c0ef57ecea6164f44ce23c6ccf5d4f0ea2f46cd
Master\Jeffrey <jef.schw@gmail.com>
2017-12-16 15:30:12 +0100

    Answer for pt 1 is too high

--- Day12/PartOne.py
+++ Day12/PartOne.py
@@ -1,7 +1,22 @@
+class Tree(object):
+
+
+    def __init__(self, name='root', children=None):
+        self.name = name
+        self.children = []
+
+    def __repr__(self):
+        return self.name
+
+    def add_child(self, node):
+        assert isinstance(node, Tree)
+        self.children.append(node)
+
+
 # INPUT
 input = open("input.txt")
 
-n = " "
+'''
 records = {}
 for _ in range(0, 2000):
     n = str(input.readline())
@@ -13,13 +28,42 @@ for _ in range(0, 2000):
     else:
         records[n_int[0]] = n_int[1]
 
-group = []
-def group_up(num):
-    children = records[num]
+print(records)
+'''
+
+records = {}
+for _ in range(0, 2000):
+    n = str(input.readline())
+    n_cleaned = n.replace("<->", "").replace(",", " ").strip().split()
+    if len(n_cleaned) > 2:
+        sublist = n_cleaned[1:]
+        records[n_cleaned[0]] = sublist
+    else:
+        records[n_cleaned[0]] = n_cleaned[1]
+
+# print(records)
+
+
+visited = []
+def find_all_the_nodes(record):
+    visited.append(record)
+    children = records[record]
     for child in children:
-        if child not in group:
-            group.append(group_up(child))
-group_up(0)
-print(group)
+        if child not in visited:
+            find_all_the_nodes(child)
+
+
+find_all_the_nodes("0")
+
+print(visited)
+print(len(visited))
+
+'''
+set_visited = list(set(visited))
+print(set_visited)
+print(len(set_visited))
+'''
+
+
 
 
